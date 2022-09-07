b326a63c667094aa143e6428c312ed19bd183932
goususlugi <94773273+goususlugi@users.noreply.github.com>
2022-09-07 22:26:57 +0300

    added homework from 9 September

---
+++ HttpClient/src/main/java/ru/kpfu/itis/Application.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis;
+
+import java.sql.SQLOutput;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
