109616da238097fc57f08ab6a85b720adbad9203
VSelyantsev <vselyantsev@mail.ru>
2022-10-08 16:08:24 +0300

    added interface and implements methods

--- 1sem/HomeworkFrom09September/HttpClient/src/main/java/ru/kpfu/itis/net/Net.java
+++ 1sem/HomeworkFrom09September/HttpClient/src/main/java/ru/kpfu/itis/net/Net.java
@@ -1,16 +1,17 @@
 package ru.kpfu.itis.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import ru.kpfu.itis.net.interfaces.HttpClient;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class Net {
+public class Net implements HttpClient {
 
     public static URL url = null;
     public static HttpURLConnection connection = null;
@@ -150,9 +151,65 @@ public class Net {
 
     }
 
+    public static String mapAsString(Map<String, String> params) {
 
+        StringBuilder result = new StringBuilder();
 
+        for (String key : params.keySet()) {
+            result.append(key + "=" + params.get(key) + "&");
+        }
 
+        return result.toString();
+    }
+
+    public static void setProperty(Map<String, String> headers) {
+        try {
+            for (String key : headers.keySet()) {
+                connection.setRequestProperty(key, headers.get(key));
+            }
+        } catch (NullPointerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String get(String u, Map<String, String> headers, Map<String, String> params) {
+        try {
+            url = new URL(u + "?" + mapAsString(params));
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            setProperty(headers);
+
+            return print(connection).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String post(String u, Map<String, String> headers, Map<String, String> params) {
+        PrintWriter pw = null;
+        try {
+            url = new URL(u);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            setProperty(headers);
+
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            pw = new PrintWriter(connection.getOutputStream());
+            pw.print(mapAsString(params));
+            pw.flush();
+
+            return print(connection).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -162,9 +219,32 @@ public class Net {
 
         //System.out.println(getMethod(inputPathForGetAndPostMethods));
         //System.out.println(postMethod(inputPathForGetAndPostMethods, token));
-        System.out.println(putMethod(inputPathForPutAndDeleteMethod, token));
+        //System.out.println(putMethod(inputPathForPutAndDeleteMethod, token));
         //System.out.println(deleteMethod(inputPathForPutAndDeleteMethod, token));
 
+        Map<String, String> headers = new HashMap<>();
+        // for get method
+        headers.put("Content-type", "application/x-www-form-urlencoded");
+        //headers.put("Accept", "application/json");
+
+        //for post method
+        headers.put("accept", "*/*");
+        headers.put("connection", "Keep-Alive");
+        headers.put("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+        headers.put("Charsert", "UTF-8");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("foo1", "bar1");
+        params.put("foo2", "bar2");
+
+        Net net = new Net();
+
+        String urlForGet = "https://postman-echo.com/get";
+        //System.out.println(net.get(urlForGet, headers, params));
+        String urlForPost = "https://postman-echo.com/post";
+        //System.out.println(net.post(urlForPost, headers, params));
+
     }
+
 
 }
